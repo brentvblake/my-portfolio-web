e7a815440f0842610bf22b72dc666e6c3ee028d3
Brent Blake <77595164+brentvblake@users.noreply.github.com>
2023-09-04 10:17:25 +0200

    path set 7

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: '/my-portfolio-web',
 			//set absolute paths for assets in production
 			//assets: dev ? '' : "/website/"
 		}
